ac5c9c9120d6937d7b79235cd49a31d53dcc76bf
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2023-05-22 07:12:26 +0900

    add troubleshooting chapter

---
+++ _extensions/bitPublish/init_fonts.R
@@ -0,0 +1,85 @@
+if(!require("Cairo")){
+  install.packages("Cairo")
+  library("Cairo")
+}
+
+if(!require("here")){
+  install.packages("here")
+  library("here")
+}
+
+if(!require("showtext")){
+  install.packages("showtext")
+  library("showtext")
+}
+
+if(!require("tidyverse")){
+  install.packages("tidyverse")
+  library("tidyverse")
+}
+
+if(!require("gt")){
+  install.packages("gt")
+  library("gt")
+}
+
+if(!require("hrbrthemes")){
+  install.packages("hrbrthemes")
+  library("hrbrthemes")
+}
+
+showtext.opts(dpi = 300)
+
+# 한글 폰트
+font_add("NanumSquare", 
+         regular = here("_extensions", "bitPublish", "fonts", "NanumSquare", 
+                        "NanumSquareR.otf"), 
+         bold = here("_extensions", "bitPublish", "fonts", "NanumSquare", 
+                     "NanumSquareB.otf"))
+
+# 영문폰트
+font_add("Nimbus Sans L",
+         regular = here("_extensions", "bitPublish", "fonts", "Nimbus Sans L", 
+                        "NimbusSanL-Reg.otf"),     
+         bold = here("_extensions", "bitPublish", "fonts", "Nimbus Sans L", 
+                     "NimbusSanL-Bol.otf"),     
+         italic = here("_extensions", "bitPublish", "fonts", "Nimbus Sans L", 
+                       "NimbusSanL-RegIta.otf"),     
+         bolditalic = here("_extensions", "bitPublish", "fonts", "Nimbus Sans L", 
+                           "NimbusSanL-BolIta.otf"))
+
+
+## 청크 옵션에서 폰트를 선택할 수 있도록 후크 추가    
+knitr::knit_hooks$set(
+  nanum = function(before, options, envir)
+    if (before) par(family = "NanumSquare"),
+  nimbus = function(before, options, envir)
+    if (before) par(family = "Nimbus Sans L")
+)
+
+knitr::opts_chunk$set(
+  message = FALSE, warning = FALSE,
+  dev = "cairo_pdf", dpi = 300, fig.showtext = TRUE, fig.pos = "htb!",
+  fig.align = "center")
+
+## ggplot2 테마 설정
+old_theme <- theme_set(theme_grey())
+theme_set(hrbrthemes::theme_ipsum(base_family = "NanumSquare"))
+
+
+## https://github.com/rstudio/gt/issues/818 참고로 작성
+## table의 cross-reference 적용을 위해서
+as_latex_with_caption <- function(gt, label, caption) {
+  gt <- gt::as_latex(gt)
+  
+  caption <- paste0(
+    "\\caption{\\label{", label, "}", caption, "}")
+  
+  latex <- strsplit(gt[1], split = "\n")[[1]]
+  latex <- c(latex[-length(latex)], caption, latex[length(latex)])
+  latex <- paste(latex, collapse = "\n")
+  gt[1] <- latex
+  
+  gt
+}
+
